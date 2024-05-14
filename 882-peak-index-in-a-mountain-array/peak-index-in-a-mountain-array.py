bf7cfd7289cb2c952a234809c684abd648022bbf
Ramdhan Ziane <issakalem@gmail.com>
2024-05-14 18:42:32 +0100

    Time: 460 ms (40.60%) | Memory: 31.1 MB (7.74%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -3,8 +3,6 @@ class Solution:
         l,r = -1,len(arr)
         while l+1<r:
             m = l + (r-l)//2
-            print(l, m, r)
-            print(arr[m-1], arr[m], arr[m+1])
             if m == 0:
                 return 1
             if m == len(arr)-1:
@@ -15,8 +13,4 @@ class Solution:
                 l = m
             else:
                 r = m
-        print(l)
-        print(r)
-        # if l==-1 or r==len(arr):
-        #     return -1
         return r
