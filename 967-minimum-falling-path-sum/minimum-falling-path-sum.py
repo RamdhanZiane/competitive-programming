8bd33e56b1a52948f6d47ebea5fab73684ce9a3b
Ramdhan Ziane <issakalem@gmail.com>
2024-08-13 20:18:33 +0100

    Time: 179 ms (8.60%) | Memory: 17.4 MB (45.80%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -0,0 +1,15 @@
+from copy import deepcopy
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        n,m=len(matrix),len(matrix[0])
+        dp=deepcopy(matrix)
+
+        # def solve(i,j):
+        #     if i==n or j==-1 or j==m:
+        #         return 0
+        #     if (i,j)
+        for i in range(n-2,-1,-1):
+            for j in range(m):
+                dp[i][j]+=min([dp[i+1][j+y] if 0<=j+y<m else float('inf') for y in range(-1,2)])
+        print(dp)
+        return min(dp[0])
