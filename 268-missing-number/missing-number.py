bc74ce2697fe6ce971c59aa86c73d1c0b17bd99e
Ramdhan Ziane <issakalem@gmail.com>
2024-08-28 18:55:02 +0100

    Time: 109 ms (68.38%) | Memory: 17.7 MB (51.90%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,7 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        
-        ans=sum(range(len(nums)+1))
-        for i in nums:
-            ans-=i
+        ans=0
+        for i in range(1,len(nums)+1):
+            ans^=i
+        for num in nums:
+            ans^=num
         return ans
